8135c5ba86aac79cdb811caa75ad233c837fe69d
Mohamadi98 <mohamadinaena23@gmail.com>
2026-05-26 15:16:25 +0300

    database schema change

--- chat-app/src/controllers/appController.ts
+++ chat-app/src/controllers/appController.ts
@@ -2,29 +2,23 @@ import express from 'express'
 import { postgresClient } from '../database/postgres'
 
 export const loginHandler: express.RequestHandler = async(req, res) => {
-    const {username, password, role} = req.body
-    let dbTable = 'coaches'
-    if(role !== 'coach') dbTable = 'clients'
+    const {username, password} = req.body
     
-    const dbRes = await postgresClient.query(`SELECT id FROM ${dbTable} WHERE name = $1 AND password = $2`, [username, password])
+    const dbRes = await postgresClient.query('SELECT id FROM users WHERE name = $1 AND password = $2', [username, password])
     if(dbRes.rows.length === 0) {
         res.sendStatus(404)
         return 
     }
     res.status(200).send({
-        'user_id': dbRes.rows[0],
-        'user_type': role 
+        'user_id': dbRes.rows[0]
     }) 
 }
 
 export const profileInfoHandler: express.RequestHandler = async(req, res) => {
     const userId = parseInt(req.query.user_id as string)
-    const userType = req.query.user_type
-    let dbTable = 'coaches'
-    if(userType === 'client') dbTable = 'clients'
 
     const dbRes = await postgresClient.query(
-        `SELECT * FROM ${dbTable} WHERE id = $1`, [userId]
+        'SELECT id, name, role, age FROM users WHERE id = $1', [userId]
     )
     if(dbRes.rowCount === 0) {
         res.sendStatus(404)
